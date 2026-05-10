6026a2588c62a9927ba6e0120b4372a9599cc493
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-05-10 23:42:49 +0530

    commit for frontend corsOrigin

--- src/main/java/com/sit/config/CorsConfig.java
+++ src/main/java/com/sit/config/CorsConfig.java
@@ -8,15 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+
+	            registry.addMapping("/**")
+	                    .allowedOrigins("http://127.0.0.1:5500") 
+	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                    .allowedHeaders("*")
+	                    .allowCredentials(true);
+	        }
+	    };
+	}
 }
